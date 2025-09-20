3ac44a7e65bf698058425b2cefd79a81a1c153d3
Sadman Shovik <sshovik28@gmail.com>
2025-09-20 11:27:40 +0000

    *  Executing task: npm run build
    > nextn@0.1.0 build
    > next build

--- next.config.ts
+++ next.config.ts
@@ -45,6 +45,14 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/studio/:path*',
+        destination: '/studio/index.html',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
