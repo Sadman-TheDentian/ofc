f51f76262f3bc716c305722c3464ab81bec01ef2
Sadman Shovik <sshovik28@gmail.com>
2025-09-20 12:41:51 +0000

    1 Create a new document type Create a new file in your Studio’s schemaTy

--- src/lib/sanity/schemas/post.ts
+++ src/lib/sanity/schemas/post.ts
@@ -9,15 +9,32 @@ export default defineType({
       name: 'title',
       title: 'Title',
       type: 'string',
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{type: 'block'}],
     }),
     defineField({
       name: 'author',
@@ -26,31 +43,12 @@ export default defineType({
       to: {type: 'author'},
     }),
     defineField({
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    }),
-    defineField({
       name: 'excerpt',
       title: 'Excerpt',
       type: 'text',
       rows: 4
     }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    }),
   ],
-
   preview: {
     select: {
       title: 'title',
