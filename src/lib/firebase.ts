7ad455cfa03af688ae916952660e8209882dbcb7
Sadman Shovik <sshovik28@gmail.com>
2025-09-20 00:32:02 +0000

    same issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAoioWUREDPmwLaZzjFqKzs0hfq_VfzWhM",
   authDomain: "studio-9852301761-76ee4.firebaseapp.com",
   projectId: "studio-9852301761-76ee4",
-  storageBucket: "studio-9852301761-76ee4.firebasestorage.app",
+  storageBucket: "studio-9852301761-76ee4.appspot.com",
   messagingSenderId: "211779146745",
   appId: "1:211779146745:web:e9f9ecac8afa545c76a328"
 };
