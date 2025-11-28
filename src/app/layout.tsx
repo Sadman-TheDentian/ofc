8b790f69b6f79d9011dfe37a47709bd0a4f786c5
Sadman Shovik <sshovik28@gmail.com>
2025-11-28 11:40:49 +0000

    remove loading animation man. not need that we just just trying hero tex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Footer from "@/components/layout/Footer";
 import { AuthProvider } from "@/lib/auth";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
 import StructuredData from "@/components/StructuredData";
-import SplashScreen from "@/components/layout/SplashScreen";
 
 const logoUrl =
   'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEirwhyibjl-3Guf8S6G442OtQmAdOzHrTcxPAuK6QxCGcAJ2I88K7Ee9DN-k_SONDddf2FeB4SwHO8l29PZ9HvHHlxJxiPDnfgrY1DBS60HsVaYv0uOAi08fm6KyrwhM7HPQhbQhL5ufVU_efX268tXM4rR8Vwok_UqbSar_b-B4btAigP5BFaU12PCjUE/s320/DENTI.SYSTEMS%20PNJ.png';
@@ -76,7 +75,6 @@ export default function RootLayout({
         )}
         suppressHydrationWarning={true}
       >
-        <SplashScreen />
         <FirebaseClientProvider>
           <AuthProvider>
             <Header />
