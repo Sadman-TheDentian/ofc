eeef69ea3966766fd806480a2caf68bfe468453d
Sadman Shovik <sshovik28@gmail.com>
2025-11-30 12:41:49 +0000

    Try fixing this error: `Server Error: Error: (0 , react__WEBPACK_IMPORTE

--- next.config.js
+++ next.config.js
@@ -1,44 +1,34 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'blogger.googleusercontent.com',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'cdn.sanity.io',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'picsum.photos',
-          port: '',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        config.externals.push('framer-motion');
-      }
-      return config;
-    },
-  };
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'blogger.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverComponentsExternalPackages: ['framer-motion'],
 };
 
 module.exports = nextConfig;
