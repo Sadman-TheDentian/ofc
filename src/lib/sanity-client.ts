e0b14888e148956d13a2e5a6c811045206c2e42c
Sadman Shovik <sshovik28@gmail.com>
2025-11-30 21:49:18 +0000

    03:45:48.133 Running build in Portland, USA (West) – pdx1 03:45:48.133 B

--- src/lib/sanity-client.ts
+++ src/lib/sanity-client.ts
@@ -1,10 +1,11 @@
+
 import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-01-01';
+export const projectId = '9x2bbr6u';
+export const dataset = 'production';
+export const apiVersion = '2024-01-01';
 export const useCdn = process.env.NODE_ENV === 'production';
 
 export const client = createClient({
