5c3a84d7bd162245622c22566cc4f995db1f94d7
Sadman Shovik <sshovik28@gmail.com>
2025-11-30 19:40:44 +0000

    how did you find those tools code? i just told you to add subdomain so t

--- tools/passwordleaker/README.md
+++
@@ -1,3 +0,0 @@
-# PasswordLeaker PRO by DentiSystems
-
-This is a standalone tool that is part of the DentiSystems cybersecurity suite.
--- tools/passwordleaker/src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import MonitoringDashboard from '../components/MonitoringDashboard';
-
-const Dashboard = () => {
-  return <MonitoringDashboard />;
-};
-
-export default Dashboard;
