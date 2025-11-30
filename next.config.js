dfa93bfc80b93254f74430981fbcde25ded7e574
Sadman Shovik <sshovik28@gmail.com>
2025-11-30 12:22:49 +0000

    Build Error Failed to compile
    Next.js (14.2.5) is outdated (learn more)

--- next.config.js
+++ next.config.js
@@ -1,33 +1,46 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'blogger.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+const nextConfig = (phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'blogger.googleusercontent.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'cdn.sanity.io',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'picsum.photos',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+    webpack: (config, { isServer }) => {
+      // This is the correct configuration to externalize framer-motion
+      // for the server build, which resolves the "export *" error.
+      if (isServer) {
+        config.externals.push('framer-motion');
+      }
+      return config;
+    },
+  };
 };
 
 module.exports = nextConfig;
